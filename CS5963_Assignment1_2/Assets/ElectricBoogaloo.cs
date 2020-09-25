222212c49f65e7c61bda1b240711df4371852b39
JohnPeterson3 <johnny.student.1@gmail.com>
2020-09-25 11:46:49 -0600

    Beginning script for electric boogaloo.

---
+++ CS5963_Assignment1_2/Assets/ElectricBoogaloo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricBoogaloo : MonoBehaviour
+{
+    public GameObject Collider1;
+    public GameObject Collider2;
+    public GameObject Collider3;
+    public GameObject Collider4;
+    public GameObject Light1;
+    public GameObject Light2;
+    public GameObject Light3;
+    public GameObject Light4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
